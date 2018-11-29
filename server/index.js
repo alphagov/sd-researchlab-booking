690033f47b0d8c338bb9de2c50608c5a12730cb5
Adrian Durran <adrian@intellidroid.eu>
2018-11-29 20:06:19 +0000

    chore: added server keys and mongoose connection

--- server/index.js
+++ server/index.js
@@ -5,6 +5,22 @@ const passport = require('passport');
 
 const morgan = require('morgan');
 
+const keys = require('./config/keys');
+
+// connect to the database
+mongoose
+  .connect(
+    process.env.MONGODB_URI,
+    {
+      useNewUrlParser: true,
+      poolSize: 10,
+      useFindAndModify: false,
+      useCreateIndex: true
+    }
+  )
+  .then(() => console.log('Database connected'))
+  .catch((error) => console.error('Unable to connect to database', error));
+
 const app = express();
 
 app.use(morgan('dev'));
@@ -13,6 +29,8 @@ app.use(helmet());
 
 const PORT = process.env.PORT || 4050;
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server started on PORT: ${PORT}`);
+});
 
 module.exports = app;
