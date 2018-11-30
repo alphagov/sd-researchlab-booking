3f7656c8af4721e7eb3183eced89b03123a68b80
adriandurran <adriandurran@users.noreply.github.com>
2018-11-30 15:19:47 +0000

    Update test.js

--- server/test/test.js
+++ server/test/test.js
@@ -5,9 +5,18 @@ const expect = chai.expect;
 
 const User = require('../models/User');
 
-describe('Database Tests', () => {
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal(-1, [1, 2, 3].indexOf(4));
+    });
+  });
+});
+
+describe('Database Tests', function() {
   // create a sandbox database connection
-  before((done) => {
+  before(function(done) {
     mongoose
       .connect(
         'mongodb://booking_db:27017/booking_tests',
@@ -25,9 +34,23 @@ describe('Database Tests', () => {
       .catch((error) =>
         console.error('unable to connect to Test Database', error)
       );
+
+    describe('Test Database models', function() {
+      // save a dummy user
+      it('New User saved to the Database', function(done) {
+        let testUser = User({
+          firstName: 'John',
+          lastName: 'Doe',
+          email: 'john@doe.com',
+          phone: '1234567',
+          password: 'password'
+        });
+        testUser.save(done);
+      });
+    });
   });
   //After all tests are finished drop database and close connection
-  after((done) => {
+  after(function(done) {
     mongoose.connection.db.dropDatabase(() => {
       mongoose.connection.close(done);
     });
