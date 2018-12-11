a0d3d9b7347903020b6b6aa9550db0b913f8c508
adriandurran <adriandurran@users.noreply.github.com>
2018-12-11 14:33:52 +0000

    feat: basic client setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from 'react-router-dom';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4050/graphql',
+  fetchOptions: {
+    credentials: 'include'
+  },
+  request: (operation) => {
+    const token = localStorage.getItem('rlbtoken');
+    operation.setContext({
+      headers: {
+        authorization: token
+      }
+    });
+  },
+  onError: ({ networkError }) => {
+    if (networkError) {
+      console.log('Network error', networkError);
+      if (networkError.status === 401) {
+        localStorage.removeItem('rlbtoken');
+      }
+    }
+  }
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
