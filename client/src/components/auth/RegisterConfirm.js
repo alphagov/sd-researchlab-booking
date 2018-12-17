a688b020c7ba33646787d4fc8855fd12cbbcbe89
adriandurran <adriandurran@users.noreply.github.com>
2018-12-17 11:56:33 +0000

    refactor: update registration confirmation page

--- client/src/components/auth/RegisterConfirm.js
+++ client/src/components/auth/RegisterConfirm.js
@@ -3,7 +3,17 @@ import { withRouter } from 'react-router-dom';
 
 const RegisterConfirm = ({ match }) => {
   console.log(match.params);
-  return <div>Registration Confirmation</div>;
+  return (
+    <div className="App">
+      <h2>Registration Confirmation</h2>
+      <p>
+        A confirmation link has been sent to your email account please click on
+        this link to complete your registration
+      </p>
+      <p>Didn't receive the email?</p>
+      <button className="button-primary">Send again</button>
+    </div>
+  );
 };
 
 export default withRouter(RegisterConfirm);
