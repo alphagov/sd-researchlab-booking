77a5d013051e4699eda176757ea16d105f31913d
Adrian Durran <adrian@intellidroid.eu>
2019-01-06 20:19:59 +0000

    refactor: tidy up DateTime comp

--- client/src/components/booking/DateTime.js
+++ client/src/components/booking/DateTime.js
@@ -3,17 +3,29 @@ import moment from 'moment';
 import styles from './DateTime.module.css';
 
 class DateTime extends Component {
-  constructor() {
-    super();
+  state = {
+    yesterday: null,
+    defaultDate: null
+  };
 
-    this.state = {
-      yesterday: moment().subtract(1, 'day'),
-      defaultDate: moment()
-        .minute(Math.floor(moment().minute() / 15) * 15)
-        .second(0)
-        .add(2, 'hours')
-    };
+  componentDidMount() {
+    this.getDefaultDate();
+    this.getYesterday();
   }
+
+  getDefaultDate = () => {
+    const defaultDate = moment()
+      .minute(Math.floor(moment().minute() / 15) * 15)
+      .second(0)
+      .add(2, 'hours');
+
+    this.setState({ defaultDate: defaultDate.toDate() });
+  };
+
+  getYesterday = () => {
+    const yesterday = moment().subtract(1, 'day');
+    this.setState({ yesterday: yesterday.toDate() });
+  };
 
   isValid = (current) => {
     return current.isAfter(this.state.yesterday);
@@ -27,16 +39,9 @@ class DateTime extends Component {
         <h2>Select a Date and Time to Book a Research Lab</h2>
         <form className={styles.bookingForm}>
           <label htmlFor="bookingDate">Select date</label>
-          <input
-            type="date"
-            name="bookingDate"
-            defaultValue={defaultDate.toDate()}
-          />
+          <input type="date" name="bookingDate" defaultValue={defaultDate} />
           <label htmlFor="bookingTime">Select Time</label>
-          <input
-            type="time"
-            defaultValue={moment(defaultDate.toDate(), 'HH:mm')}
-          />
+          <input type="time" defaultValue={moment(defaultDate, 'HH:mm')} />
           <label htmlFor="duration">Duration (hours)</label>
           <input type="number" name="duration" defaultValue={1} />
           <button>Book</button>
