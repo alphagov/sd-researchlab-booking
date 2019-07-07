e5cc18a6211fc31f39b88f0ca178b71d8a3af206
Adrian Durran <adrian@intellidroid.eu>
2019-07-07 08:08:41 +0100

    fixed rendering bug in LabCalendars

--- client/src/components/calendars/LabCalendar.js
+++ client/src/components/calendars/LabCalendar.js
@@ -5,6 +5,12 @@ import LabCalendarHeader from './LabCalendarHeader';
 import LabCalendarDays from './LabCalendarDays';
 import LabCalendarCells from './LabCalendarCells';
 
+import { Query } from 'react-apollo';
+import { GET_CALENDAR_FREE_BUSY } from '../../queries';
+
+import Spinner from '../shared/Spinner';
+import Error from '../../containers/Error';
+
 import styles from '../../css/LabCalendar.module.css';
 
 const LabCalendar = ({ calendar }) => {
@@ -20,6 +26,11 @@ const LabCalendar = ({ calendar }) => {
     setMonth(dateFns.addMonths(currentMonth, 1));
   };
 
+  const monthStart = dateFns.startOfMonth(currentMonth);
+  const monthEnd = dateFns.endOfMonth(monthStart);
+  const startDate = dateFns.startOfWeek(monthStart, { weekStartsOn: 1 });
+  const endDate = dateFns.endOfWeek(monthEnd);
+
   return (
     <>
       <h3 className="govuk-heading-m">{resourceName}</h3>
@@ -30,7 +41,32 @@ const LabCalendar = ({ calendar }) => {
           nextMonth={nextMonth}
         />
         <LabCalendarDays month={currentMonth} />
-        <LabCalendarCells month={currentMonth} calendar={calendar} />
+
+        <Query
+          query={GET_CALENDAR_FREE_BUSY}
+          variables={{
+            start: startDate,
+            end: endDate,
+            items: calendar.resourceEmail
+          }}
+          pollInterval={60000}
+        >
+          {({ loading, error, data }) => {
+            if (loading) return <Spinner />;
+            if (error) return <Error error={error} />;
+
+            const { busy } = data.getCalendarFreeBusyList.calendars[0];
+            const dateFormat = 'DD';
+
+            const busyDays = busy.map((bDay) =>
+              dateFns.format(bDay.start, dateFormat)
+            );
+
+            return (
+              <LabCalendarCells month={currentMonth} busyDays={busyDays} />
+            );
+          }}
+        </Query>
       </div>
     </>
   );
--- client/src/components/calendars/LabCalendarCells.js
+++ client/src/components/calendars/LabCalendarCells.js
@@ -2,13 +2,7 @@ import React, { useState } from 'react';
 import dateFns from 'date-fns';
 import styles from '../../css/LabCalendar.module.css';
 
-import { Query } from 'react-apollo';
-import { GET_CALENDAR_FREE_BUSY } from '../../queries';
-
-import Spinner from '../shared/Spinner';
-import Error from '../../containers/Error';
-
-const LabCalendarCells = ({ month, calendar }) => {
+const LabCalendarCells = ({ month, busyDays }) => {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const monthStart = dateFns.startOfMonth(month);
   const monthEnd = dateFns.endOfMonth(monthStart);
@@ -21,63 +15,42 @@ const LabCalendarCells = ({ month, calendar }) => {
   const onDateClick = (day) => {
     setSelectedDate(day);
   };
-  return (
-    <Query
-      query={GET_CALENDAR_FREE_BUSY}
-      variables={{
-        start: startDate,
-        end: endDate,
-        items: calendar.resourceEmail
-      }}
-    >
-      {({ loading, error, data }) => {
-        if (loading) return <Spinner />;
-        if (error) return <Error error={error} />;
 
-        const { busy } = data.getCalendarFreeBusyList.calendars[0];
+  let days = [];
+  let day = startDate;
+  let formattedDate = '';
+  while (day <= endDate) {
+    for (let i = 0; i < 7; i++) {
+      formattedDate = dateFns.format(day, dateFormat);
+      const cloneDay = day;
 
-        const busyDays = busy.map((bDay) =>
-          dateFns.format(bDay.start, dateFormat)
-        );
+      days.push(
+        <div
+          className={`${styles.col} ${styles.cell} ${
+            !dateFns.isSameMonth(day, monthStart)
+              ? `${styles.disabled}`
+              : dateFns.isSameDay(day, selectedDate)
+              ? `${styles.selected}`
+              : ''
+          } ${busyDays.includes(formattedDate) && styles.disabledBusy}`}
+          key={day}
+          onClick={() => onDateClick(dateFns.parse(cloneDay))}
+        >
+          <span className={styles.number}>{formattedDate}</span>
+          <span className={styles.bg}>{formattedDate}</span>
+        </div>
+      );
 
-        let days = [];
-        let day = startDate;
-        let formattedDate = '';
-        while (day <= endDate) {
-          for (let i = 0; i < 7; i++) {
-            formattedDate = dateFns.format(day, dateFormat);
-            const cloneDay = day;
-
-            days.push(
-              <div
-                className={`${styles.col} ${styles.cell} ${
-                  !dateFns.isSameMonth(day, monthStart)
-                    ? `${styles.disabled}`
-                    : dateFns.isSameDay(day, selectedDate)
-                    ? `${styles.selected}`
-                    : ''
-                } ${busyDays.includes(formattedDate) && styles.disabledBusy}`}
-                key={day}
-                onClick={() => onDateClick(dateFns.parse(cloneDay))}
-              >
-                <span className={styles.number}>{formattedDate}</span>
-                <span className={styles.bg}>{formattedDate}</span>
-              </div>
-            );
-
-            day = dateFns.addDays(day, 1);
-          }
-          rows.push(
-            <div className={styles.row} key={day}>
-              {days}
-            </div>
-          );
-          days = [];
-        }
-        return <div className={styles.body}>{rows}</div>;
-      }}
-    </Query>
-  );
+      day = dateFns.addDays(day, 1);
+    }
+    rows.push(
+      <div className={styles.row} key={day}>
+        {days}
+      </div>
+    );
+    days = [];
+  }
+  return <div className={styles.body}>{rows}</div>;
 };
 
 export default LabCalendarCells;
