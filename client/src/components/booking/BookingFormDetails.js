5c4641a707c3aac48f06160845bfc6f4a74e3790
Adrian Durran <adrian@intellidroid.eu>
2019-07-09 09:03:53 +0100

    added booking details form

--- client/src/components/booking/BookingFormDetails.js
+++ client/src/components/booking/BookingFormDetails.js
@@ -1,8 +1,57 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+
+const initialState = {
+  moreDetail: { value: '', valid: true, reason: '' }
+};
 
 const BookingFormDetails = ({ history }) => {
-  return <div>Booking form details</div>;
+  const [values, handleChange] = useForm(initialState);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(values);
+    // if everything works ok move to next part of form with
+    // summary of booking
+    // history.push('/book-a-research-lab/booking-details');
+  };
+  return (
+    <div className="govuk-grid-column-full">
+      <div className="govuk-form-group">
+        <fieldset className="govuk-fieldset">
+          <legend className="govuk-fieldset__legend govuk-fieldset__legend--l">
+            <h2 className="govuk-fieldset__heading">
+              Enter a brief description of the research session
+            </h2>
+          </legend>
+          <form onSubmit={(event) => handleSubmit(event)}>
+            <div style={{ marginBottom: '2rem' }}>
+              <div className="govuk-form-group">
+                <label className="govuk-label" htmlFor="moreDetail">
+                  Can you provide more detail?
+                </label>
+                <span id="more-detail-hint" className="govuk-hint">
+                  Do not include personal or financial information, like your
+                  National Insurance number or credit card details.
+                </span>
+                <textarea
+                  className="govuk-textarea"
+                  id="more-detail"
+                  name="moreDetail"
+                  rows="5"
+                  aria-describedby="more-detail-hint"
+                />
+              </div>
+            </div>
+            <button type="submit" className="govuk-button">
+              Book the lab
+            </button>
+          </form>
+        </fieldset>
+      </div>
+    </div>
+  );
 };
 
 export default withRouter(BookingFormDetails);
--- client/src/components/booking/BookingFormName.js
+++ client/src/components/booking/BookingFormName.js
@@ -18,97 +18,98 @@ const BookingFormName = ({ history }) => {
     history.push('/book-a-research-lab/booking-details');
   };
 
-  // console.log(props);
-
   return (
-    <div className="govuk-grid-column-two-thirds">
-      <h2 className="govuk-heading-l">Lab booking form</h2>
-      <fieldset className="govuk-fieldset">
-        <legend className="govuk-fieldset__legend govuk-fieldset__legend--m">
-          <h3 className="govuk-fieldset__heading">
-            Enter details of the person this booking is for
-          </h3>
-        </legend>
-        <form onSubmit={(event) => handleSubmit(event)}>
-          <div
-            className={`govuk-form-group ${!values.firstName.valid &&
-              `govuk-form-group--error`}`}
-          >
-            <label htmlFor="firstName" className="govuk-label">
-              First name
-            </label>
-            {!values.firstName.valid && (
-              <span id="event-name-error" className="govuk-error-message">
-                <span className="govuk-visually-hidden">Error:</span> Enter your
-                first name
-              </span>
-            )}
-            <input
-              type="text"
-              className={`govuk-input govuk-!-width-two-thirds ${!values
-                .firstName.valid && `govuk-input--error`}`}
-              name="firstName"
-              id="firstName"
-              onChange={handleChange}
-              value={values.firstName.value}
-            />
-          </div>
-          <div
-            className={`govuk-form-group ${!values.lastName.valid &&
-              `govuk-form-group--error`}`}
-          >
-            <label htmlFor="lastName" className="govuk-label">
-              Last name
-            </label>
-            {!values.lastName.valid && (
-              <span id="event-name-error" className="govuk-error-message">
-                <span className="govuk-visually-hidden">Error:</span> Enter your
-                last name
-              </span>
-            )}
-            <input
-              type="text"
-              className={`govuk-input govuk-!-width-two-thirds ${!values
-                .lastName.valid && `govuk-input--error`}`}
-              name="lastName"
-              id="lastName"
-              onChange={handleChange}
-              value={values.lastName.value}
-            />
-          </div>
-          <div
-            className={`govuk-form-group ${!values.email.valid &&
-              `govuk-form-group--error`}`}
-          >
-            <label htmlFor="email" className="govuk-label">
-              Email
-            </label>
-            <span id="email-hint" className="govuk-hint">
-              Contact email address for session owner.
-            </span>
-            {!values.email.valid && (
-              <span id="event-name-error" className="govuk-error-message">
-                <span className="govuk-visually-hidden">Error:</span> Enter your
-                email address
-              </span>
-            )}
-            <input
-              type="email"
-              className={`govuk-input ${!values.email.valid &&
-                `govuk-input--error`}`}
-              name="email"
-              id="email"
-              onChange={handleChange}
-              value={values.email.value}
-            />
-          </div>
-          <button type="submit" className="govuk-button">
-            Save and continue
-          </button>
-        </form>
-      </fieldset>
+    <div className="govuk-grid-column-full">
+      <div className="govuk-form-group">
+        <fieldset className="govuk-fieldset">
+          <legend className="govuk-fieldset__legend govuk-fieldset__legend--l">
+            <h2 className="govuk-fieldset__heading">
+              Enter details of the person this booking is for
+            </h2>
+          </legend>
+          <form onSubmit={(event) => handleSubmit(event)}>
+            <div style={{ marginBottom: '2rem' }}>
+              <div
+                className={`govuk-form-group ${!values.firstName.valid &&
+                  `govuk-form-group--error`}`}
+              >
+                <label htmlFor="firstName" className="govuk-label">
+                  First name
+                </label>
+                {!values.firstName.valid && (
+                  <span id="event-name-error" className="govuk-error-message">
+                    <span className="govuk-visually-hidden">Error:</span> Enter
+                    your first name
+                  </span>
+                )}
+                <input
+                  type="text"
+                  className={`govuk-input govuk-!-width-two-thirds ${!values
+                    .firstName.valid && `govuk-input--error`}`}
+                  name="firstName"
+                  id="firstName"
+                  onChange={handleChange}
+                  value={values.firstName.value}
+                />
+              </div>
+              <div
+                className={`govuk-form-group ${!values.lastName.valid &&
+                  `govuk-form-group--error`}`}
+              >
+                <label htmlFor="lastName" className="govuk-label">
+                  Last name
+                </label>
+                {!values.lastName.valid && (
+                  <span id="event-name-error" className="govuk-error-message">
+                    <span className="govuk-visually-hidden">Error:</span> Enter
+                    your last name
+                  </span>
+                )}
+                <input
+                  type="text"
+                  className={`govuk-input govuk-!-width-two-thirds ${!values
+                    .lastName.valid && `govuk-input--error`}`}
+                  name="lastName"
+                  id="lastName"
+                  onChange={handleChange}
+                  value={values.lastName.value}
+                />
+              </div>
+              <div
+                className={`govuk-form-group ${!values.email.valid &&
+                  `govuk-form-group--error`}`}
+              >
+                <label htmlFor="email" className="govuk-label">
+                  Email
+                </label>
+                <span id="email-hint" className="govuk-hint">
+                  Contact email address for session owner.
+                </span>
+                {!values.email.valid && (
+                  <span id="event-name-error" className="govuk-error-message">
+                    <span className="govuk-visually-hidden">Error:</span> Enter
+                    your email address
+                  </span>
+                )}
+                <input
+                  type="email"
+                  className={`govuk-input ${!values.email.valid &&
+                    `govuk-input--error`}`}
+                  name="email"
+                  id="email"
+                  onChange={handleChange}
+                  value={values.email.value}
+                />
+              </div>
+            </div>
+            <button type="submit" className="govuk-button">
+              Save and continue
+            </button>
+          </form>
+        </fieldset>
+      </div>
     </div>
   );
 };
 
-export default BookingFormName;
+export default withRouter(BookingFormName);
