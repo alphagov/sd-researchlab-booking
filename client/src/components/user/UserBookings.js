dc477690c1baf7f0a738a1f5239d0a7e81f53c9b
Adrian Durran <adrian@intellidroid.eu>
2019-08-12 11:24:44 +0100

    client - adding information to the user booking summary

--- client/src/components/user/UserBookings.js
+++ client/src/components/user/UserBookings.js
@@ -4,7 +4,15 @@ import { timeInWords, readableDate, dateIsPast } from '../../utils/dateUtils';
 import styles from '../../css/UserBooking.module.css';
 
 const UserBookings = ({ booking }) => {
-  const { eventTitle, eventStatus, eventStart } = booking;
+  const {
+    eventTitle,
+    eventStatus,
+    eventStart,
+    eventEnd,
+    eventDescription,
+    guests,
+    equipment
+  } = booking;
   return (
     <div className="govuk-grid-column-one-half">
       <dl className={`govuk-summary-list ${styles.summaryList}`}>
@@ -26,6 +34,30 @@ const UserBookings = ({ booking }) => {
             } ${timeInWords(eventStart)} ${
               !dateIsPast(eventStart) ? '' : 'ago'
             })`}
+          </dd>
+        </div>
+        <div className="govuk-summary-list__row">
+          <dt className="govuk-summary-list__key">Ends</dt>
+          <dd className={`govuk-summary-list__value `}>
+            {readableDate(eventEnd)}
+          </dd>
+        </div>
+        <div className="govuk-summary-list__row">
+          <dt className="govuk-summary-list__key">Description</dt>
+          <dd className={`govuk-summary-list__value `}>{eventDescription}</dd>
+        </div>
+        <div className="govuk-summary-list__row">
+          <dt className="govuk-summary-list__key">Number of guests</dt>
+          <dd className={`govuk-summary-list__value `}>{guests}</dd>
+        </div>
+        <div className="govuk-summary-list__row">
+          <dt className="govuk-summary-list__key">Equipment needed</dt>
+          <dd className={`govuk-summary-list__value ${styles.listValue}`}>
+            <ul className="govuk-list">
+              {equipment.split(',').map((equip, i) => (
+                <li key={i}>{equip}</li>
+              ))}
+            </ul>
           </dd>
         </div>
       </dl>
